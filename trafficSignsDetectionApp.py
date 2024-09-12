194d3b5c21765b6d55b3b70bf4c0f58f197e628a
SalvoSamba01 <salvosambataro2001@gmail.com>
2024-09-12 17:08:19 +0200

    Update trafficSignsDetectionApp.py

--- trafficSignsDetectionApp.py
+++ trafficSignsDetectionApp.py
@@ -6,15 +6,12 @@ from PIL import Image, ImageTk
 from collections import Counter
 from ultralytics import YOLOv10
 
-# Definisci la dimensione massima per le immagini
 MAX_IMAGE_SIZE = (400, 400)
 
-# Funzione per ridimensionare l'immagine mantenendo il rapporto d'aspetto
 def resize_image(image, max_size):
     image.thumbnail(max_size, Image.ANTIALIAS)
     return image
 
-# Funzione per caricare l'immagine ground truth
 def load_ground_truth():
     global gt_img
     if img_path:
@@ -35,10 +32,9 @@ def load_ground_truth():
         except Exception as e:
             messagebox.showerror("Error", f"Ground truth image loading error: {str(e)}")
 
-# Funzione per caricare un'immagine
 def load_image():
     global img, img_path
-    remove_prediction()  # Rimuovi la predizione e il titolo precedente se presente
+    remove_prediction()
     img_path = filedialog.askopenfilename()
     if img_path:
         img = cv2.imread(img_path)
@@ -51,7 +47,6 @@ def load_image():
         title_left.grid()  # Mostra il titolo "Original image"
         load_ground_truth()
 
-# Funzione per rimuovere l'immagine attuale e i relativi titoli
 def remove_images():
     global img, gt_img
     img_panel_left.config(image='')
@@ -60,18 +55,16 @@ def remove_images():
     title_left.grid_remove()
     title_middle.grid_remove()
     title_right.grid_remove()
-    prediction_listbox.pack_forget()  # Nascondi la listbox delle predizioni
-    prediction_listbox.delete(0, tk.END)  # Svuota la lista delle predizioni
+    prediction_listbox.pack_forget() 
+    prediction_listbox.delete(0, tk.END)  
     img = None
     gt_img = None
 
-# Funzione per rimuovere la predizione e il titolo
 def remove_prediction():
     img_panel_right.config(image='')
     title_right.grid_remove()
-    prediction_listbox.pack_forget()  # Nascondi la listbox delle predizioni
+    prediction_listbox.pack_forget() 
 
-# Funzione per caricare il modello YOLO
 def load_model(event):
     global model
     model_name = selected_model.get()
@@ -86,7 +79,6 @@ def load_model(event):
     except Exception as e:
         messagebox.showerror("Error", f"Error in model loading: {str(e)}")
 
-# Funzione per eseguire la predizione
 def predict():
     global img
     if img is None:
@@ -97,7 +89,7 @@ def predict():
         return
 
     results = model(img_path)
-    class_count = Counter()  # Per contare le occorrenze delle classi
+    class_count = Counter()  
 
     for result in results:
         image = result.orig_img
@@ -131,28 +123,25 @@ def predict():
         img_tk = ImageTk.PhotoImage(img_pil)
         img_panel_right.config(image=img_tk)
         img_panel_right.image = img_tk
-        title_right.grid()  # Mostra il titolo "Predictions"
+        title_right.grid()  
 
-        # Aggiorna la listbox con le classi predette
-        prediction_listbox.delete(0, tk.END)  # Svuota la lista
+        prediction_listbox.delete(0, tk.END) 
         if class_count:
             prediction_listbox.insert(tk.END, "PREDICTIONS")
             for class_name, count in class_count.items():
                 prediction_listbox.insert(tk.END, f"{class_name} x{count}")
-            prediction_listbox.pack(side=tk.BOTTOM, fill=tk.X)  # Mostra la listbox delle predizioni
+            prediction_listbox.pack(side=tk.BOTTOM, fill=tk.X)
         else:
             prediction_listbox.insert(tk.END, "No traffic sign detected!")
-            prediction_listbox.pack(side=tk.BOTTOM, fill=tk.X)  # Mostra la listbox delle predizioni
+            prediction_listbox.pack(side=tk.BOTTOM, fill=tk.X)  
 
-# Configurazione dell'interfaccia utente con tkinter
 root = tk.Tk()
 root.title("Traffic Sign Detection App")
-root.update_idletasks()  # Assicurati che il layout sia aggiornato
+root.update_idletasks() 
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
 root.geometry(f"{width}x{height}+0+0")
 
-# Menu a tendina per la selezione del modello
 model_label = tk.Label(root, text="Select YOLO model:")
 model_label.pack()
 
@@ -162,19 +151,15 @@ model_menu['values'] = ('YOLOv10n', 'YOLOv10s', 'YOLOv10m')
 model_menu.pack()
 model_menu.bind("<<ComboboxSelected>>", load_model)
 
-# Frame per i pulsanti di caricamento e rimozione dell'immagine
 button_frame = tk.Frame(root)
 button_frame.pack(pady=10)
 
-# Pulsante per caricare l'immagine
 load_img_btn = tk.Button(button_frame, text="Load image", command=load_image)
 load_img_btn.pack(side=tk.LEFT, padx=5)
 
-# Pulsante per rimuovere l'immagine caricata
 remove_img_btn = tk.Button(button_frame, text="Remove image", command=remove_images)
 remove_img_btn.pack(side=tk.LEFT, padx=5)
 
-# Riquadri e titoli per le immagini
 frame = tk.Frame(root)
 frame.pack()
 
@@ -199,19 +184,15 @@ img_panel_middle.grid(row=1, column=1, padx=10, pady=10)
 img_panel_right = tk.Label(frame)
 img_panel_right.grid(row=1, column=2, padx=10, pady=10)
 
-# Listbox per mostrare le predizioni
 prediction_listbox = tk.Listbox(root, height=15, width=40, font=("Courier", 10), justify="center")
 prediction_listbox.pack_forget()  # Nascondi inizialmente
 
-# Pulsante per fare la predizione
 predict_btn = tk.Button(root, text="Predict", command=predict)
 predict_btn.pack(pady=10)
 
-# Variabili globali
 img = None
 img_path = None
 gt_img = None
 model = None
 
-# Avvio dell'interfaccia
 root.mainloop()
